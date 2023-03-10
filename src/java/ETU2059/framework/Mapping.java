75a47767a7e9e5f73cd374a3d86791fc2fa2dfc9
Tafitasoa RATSIMBAHARISON <118884621+tafitasoa1949@users.noreply.github.com>
2023-03-10 11:29:31 +0100

    partie

---
+++ src/java/ETU2059/framework/Mapping.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ETU2059.framework;
+
+/**
+ *
+ * @author Tafitasoa-P15B-140
+ */
+public class Mapping {
+    String className;
+    String method;
+}
---
+++ src/java/ETU2059/framework/servlet/FrontServlet.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package ETU2059.framework.servlet;
+
+import ETU2059.framework.Mapping;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import java.util.HashMap;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Tafitasoa-P15B-140
+ */
+@WebServlet(name = "FrontServlet", urlPatterns = {"/FrontServlet"})
+public class FrontServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    HashMap<String,Mapping> mappingUrls;
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        out.println("Servlet : FrontServlet");
+        out.println("<br>");
+        out.println("Context Path :"+request.getContextPath());
+        out.println("<br>");
+        out.println("URL :"+request.getRequestURI());
+        out.println("<br>");
+        out.println("Parametre :");
+        Enumeration<String> liste = request.getParameterNames();
+        while(liste.hasMoreElements()){
+            String element = liste.nextElement();
+            String[] elementValues = request.getParameterValues(element);
+            for(int i=0 ; i<elementValues.length ; i++){
+                out.println(element+" "+(i+1)+" : "+elementValues[i]);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
